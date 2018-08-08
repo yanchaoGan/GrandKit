4185c819b683e52388809acc08b10607134f3737
gychao <757237523@qq.com>
2018-08-08 23:07:02 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by ganyanchao on 2018/8/8.
+//  Copyright © 2018年 G.Y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
